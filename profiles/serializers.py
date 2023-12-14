f5229a9667b2ba1990074889ae31a3338df83b43
Vasileios20 <vasilis_tsim@outlook.com>
2023-12-14 19:22:57 +0000

    Add profiles detail view

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Profile
+from .models import Profile, Post
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -9,3 +9,12 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = ['id', 'owner', 'name', 'content', 'image']
         # fields = '__all__'
+
+
+class PostSerializer(serializers.ModelSerializer):
+    profile_name = serializers.ReadOnlyField(source='owner.profile.name')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    class Meta:
+        model = Post
+        fields = ['id', 'owner', 'title', 'content', 'image']
