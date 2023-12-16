11ef5d16a01ccd9ba42f34ac15a69d52e2db4746
Vasileios20 <vasilis_tsim@outlook.com>
2023-12-16 18:53:46 +0000

    Refactor ProfileList and ProfileDetail to generic views

--- profiles/views.py
+++ profiles/views.py
@@ -1,52 +1,74 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
+# from django.http import Http404
+# from rest_framework import status
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles,
-            many=True,
-            context={'request': request}
-        )
+# class ProfileList(APIView):
+#     def get(self, request):
+#         profiles = Profile.objects.all()
+#         serializer = ProfileSerializer(
+#             profiles,
+#             many=True,
+#             context={'request': request}
+#         )
 
-        return Response(serializer.data)
+#         return Response(serializer.data)
 
 
-class ProfileDetail(APIView):
+# class ProfileDetail(APIView):
+#     serializer_class = ProfileSerializer
+#     permission_classes = [IsOwnerOrReadOnly]
+
+#     def get_object(self, pk):
+#         try:
+#             profile = Profile.objects.get(pk=pk)
+#             self.check_object_permissions(self.request, profile)
+#             return profile
+#         except Profile.DoesNotExist:
+#             raise Http404
+
+#     def get(self, request, pk):
+#         profile = self.get_object(pk)
+#         serializer = ProfileSerializer(
+#             profile,
+#             context={'request': request}
+#         )
+#         return Response(serializer.data)
+
+#     def put(self, request, pk):
+#         profile = self.get_object(pk)
+#         serializer = ProfileSerializer(
+#             profile,
+#             data=request.data,
+#             context={'request': request}
+#         )
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProfileList(generics.ListCreateAPIView):
+    """
+    List all profiles
+    No create method because profiles are created
+    automatically by django signals
+    """
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    permission_classes = [IsOwnerOrReadOnly]
 
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
 
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile,
-            context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile,
-            data=request.data,
-            context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Concrete view for retrieving, updating or deleting a model instance.
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [
+        IsOwnerOrReadOnly
+    ]
