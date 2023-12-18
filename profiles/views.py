a86ba689bdf70f9dc6dc1ab2bf85c0541342f83e
Vasileios20 <vasilis_tsim@outlook.com>
2023-12-18 19:35:15 +0000

    Add search filter and filterset to ProfileList

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 # from django.http import Http404
 # from rest_framework import status
 # from rest_framework.views import APIView
@@ -67,7 +68,11 @@ class ProfileList(generics.ListCreateAPIView):
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile'
     ]
     ordering_fields = [
         'posts_count',
