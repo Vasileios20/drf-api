eda6f8d086f1d7b5165ba4f23e6d9859640db8cb
vasileios20 <vasilis_tsim@outlook.com>
2023-12-20 14:41:13 +0000

    Add CLIENT_ORIGIN_DEV to settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -61,7 +62,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ['ALLOWED_HOSTS']]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get['ALLOWED_HOSTS']]
 
 
 # Application definition
@@ -109,9 +110,12 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
+    ).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.http://172.28.203.110$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
