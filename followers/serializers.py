3786ad6ecdfd8ddd81603a70e3b767cf1e67b9bb
vasileios20 <vasilis_tsim@outlook.com>
2024-01-02 19:55:54 +0000

    Fix typo to followers serializers

--- followers/serializers.py
+++ followers/serializers.py
@@ -8,17 +8,16 @@ class FollowerSerializer(serializers.ModelSerializer):
     Serializer for the Follower model
     Create method handles the unique constraint on 'owner' and 'followed'
     """
-    owner = serializers.ReadOnlyField(source='owner.username')
-    followed = serializers.ReadOnlyField(source='followed.username')
+
+    owner = serializers.ReadOnlyField(source="owner.username")
+    followed_name = serializers.ReadOnlyField(source="followed.username")
 
     def create(self, validated_data):
         try:
             return super().create(validated_data)
         except IntegrityError:
-            raise serializers.ValidationError({
-                'detail: possible duplicate entry'
-            })
+            raise serializers.ValidationError({"detail: possible duplicate entry"})
 
     class Meta:
         model = Follower
-        fields = ['id', 'owner', 'followed', 'created_at']
+        fields = ["id", "owner", "followed", "created_at", "followed_name"]
